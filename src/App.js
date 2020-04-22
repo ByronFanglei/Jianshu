8ac9b0ff7789216a256d2d2e9083eaa005b7d4dc
ByronFanglei <984724098@qq.com>
2020-04-22 23:30:44 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React,{ Component } from 'react';
+import React,{ Component,Fragment } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import { GlobalStyle } from './style';
 import { GlobalIcon } from './statics/iconfont/iconfont';
 import Header from './common/header';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return(
       <Provider store={store}>
+        <Fragment>
           <GlobalStyle />
           <GlobalIcon />
           <Header />
+          <BrowserRouter>
+            <Fragment>
+              <Route path='/'exact render={(() => <div>home</div>)}></Route>
+              <Route path='/detail' exact render={(() => <div>detail</div>)}></Route>
+            </Fragment>
+          </BrowserRouter>
+        </Fragment>
       </Provider>
     )
   }
